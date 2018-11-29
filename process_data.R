25e4ccc6004736c1ff65c99802e15290740f0e1d
Olivia Li <oliviali013@gmail.com>
2018-11-29 12:46:17 -0800

    Plant_species2

--- process_data.R
+++ process_data.R
@@ -14,9 +14,11 @@ angiosperms_list <- as.data.frame(read.csv("angiosperms.csv", header = TRUE))
 colnames(angiosperms_list) <- "plants"
 levels(angiosperms_list$plants) <- tolower(levels(angiosperms_list$plants))
 
+practice_species2 <- practice_species[practice_species$V1 %in% practice_species$V1[duplicated(practice_species$V1)],]
+
 library(dplyr)
-practice_species %>%
+practice_species2 %>%
   filter(V2%in% ant_list$ants)
 
-practice_species %>%
+practice_species2 %>%
   filter(V2%in% angiosperms_list$plants)
