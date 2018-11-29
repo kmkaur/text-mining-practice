d27e344ad4164b8c9fe12500f7a8d6db6cbb2c4b
Katrina Kaur <kaur@zoology.ubc.ca>
2018-11-29 12:00:11 -0800

    importing lists

--- process_data.R
+++ process_data.R
@@ -1,3 +1,13 @@
 #trying to process text-mined data
 
-#new line of code
+#set your working directory here
+setwd("~/Desktop")
+
+practice_species <- read.csv("test_species.csv", header=FALSE)
+
+ant_list <- read.csv("worldants_specieslist.csv", header=TRUE)
+ant_list <- as.data.frame(ant_list$current.valid.name)
+colnames(ant_list) <- "ants"
+
+angiosperms_list <- read.csv("angiosperms.csv", header = TRUE)
+
