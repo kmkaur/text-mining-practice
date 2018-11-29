87a0709ef2ec4b3b0720b1b59bf58f0dfe07445e
Katrina Kaur <kaur@zoology.ubc.ca>
2018-11-29 12:25:39 -0800

    filtering

--- process_data.R
+++ process_data.R
@@ -8,6 +8,16 @@ practice_species <- read.csv("test_species.csv", header=FALSE)
 ant_list <- read.csv("worldants_specieslist.csv", header=TRUE)
 ant_list <- as.data.frame(ant_list$current.valid.name)
 colnames(ant_list) <- "ants"
+levels(ant_list$ants) <- tolower(levels(ant_list$ants))
 
-angiosperms_list <- read.csv("angiosperms.csv", header = TRUE)
+angiosperms_list <- as.data.frame(read.csv("angiosperms.csv", header = TRUE))
+colnames(angiosperms_list) <- "plants"
+levels(angiosperms_list$plants) <- tolower(levels(angiosperms_list$plants))
+
+library(dplyr)
+practice_species %>%
+  filter(V2%in% ant_list$ants)
+
+practice_species %>%
+  filter(V2%in% angiosperms_list$current.valid.name)
 
